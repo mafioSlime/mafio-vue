68b71149f179de1ddae44980468b4722271f3b99
John Paguirigan <johnjaroldxvi@gmail.com>
2020-02-25 18:45:11 -0700

    added if and for

---
+++ looping/app.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    name: 'tsuna',
+    characters: ['Mario', 'Bowser', 'Luigi', 'Yoshi', ],
+    ninjas: [{
+        name: "Ryu",
+        age: 25
+      },
+      {
+        name: "yoshi",
+        age: 35
+      },
+      {
+        name: 'Ken',
+        age: 55
+      }
+    ]
+  }
+})
